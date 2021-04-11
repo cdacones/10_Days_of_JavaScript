f6abdc5981a48ab7ee7bcea1f88002477d9cc98d
charmaine <daconesc17@gmail.com>
2021-04-11 02:22:39 -0700

    Update file

---
+++ hello-world.js
@@ -0,0 +1,9 @@
+// Create a function called greeting with one parameter of type string called parameterVariable
+function greeting(parameterVariable) {
+  // Use standard output to print Hello, World! to the console
+  console.log("Hello, World!");
+  // Print the contents of the argument passed to the function
+  console.log(parameterVariable);
+}
+
+
